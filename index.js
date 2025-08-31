e4e28cfa028e8532bb4b977d12f4f4dea76b321d
Daniel <danielk.97@outlook.com>
2025-08-31 13:06:37 +1000

    Apply prettier format changes

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
 /**
  * Fetches film data from SWAPI
- * 
+ *
  * @param {number} episodeNum Episode number to get data for
  */
 async function fetchData(episodeNum) {
-  const response = await fetch(`https://swapi.info/api/films/${episodeNum}`);
+  const response = await fetch(`https://swapi.info/api/films/${episodeNum}`)
   if (!response.ok) {
-    throw new Error("Something went wrong!");
+    throw new Error('Something went wrong!')
   }
-  const responseData = await response.json();
-  return responseData;
+  const responseData = await response.json()
+  return responseData
 }
 
 /**
  * Plays opening crawl
- * 
+ *
  * @param {number} episodeNum Episode number to play crawl from
  */
 async function playCrawl(episodeNum) {
   // Start playing audio
-  const audioEl = document.getElementById("audio");
-  audioEl.play();
+  const audioEl = document.getElementById('audio')
+  audioEl.play()
 
   // Convert episode number to roman numeral
-  const romanNumEpisode = convertToRomanNumeral(episodeNum);
+  const romanNumEpisode = convertToRomanNumeral(episodeNum)
 
   // Get film data
-  const responseData = await fetchData(episodeNum);
-  console.log(responseData);
+  const responseData = await fetchData(episodeNum)
+  console.log(responseData)
 
   // Hide buttons
-  document.getElementById("container").style.display = "none";
-  document.getElementById("ep-card-container").style.display = "none";
-  document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+  document.getElementById('container').style.display = 'none'
+  document.getElementById('ep-card-container').style.display = 'none'
+  document.getElementsByTagName('body')[0].style.overflowY = 'hidden'
 
   // Show fade, intro, logo
-  document.getElementById("intro").style.display = "flex";
-  document.getElementById("logo").style.display = "flex";
-  document.getElementById("fade").style.zIndex = "1";
+  document.getElementById('intro').style.display = 'flex'
+  document.getElementById('logo').style.display = 'flex'
+  document.getElementById('fade').style.zIndex = '1'
 
   // Remove intro text and logo from DOM after timeout
   setTimeout(() => {
-    document.getElementById("intro").style.display = "none";
+    document.getElementById('intro').style.display = 'none'
     setTimeout(() => {
-      document.getElementById("logo").style.display = "none";
-      document.getElementById("fade").style.display = "block";
-    }, 6000);
-  }, 9000);
+      document.getElementById('logo').style.display = 'none'
+      document.getElementById('fade').style.display = 'block'
+    }, 6000)
+  }, 9000)
 
-    const crawlElement = document.getElementById("crawl");
-    crawlElement.classList.add("crawl");
-    const titleEl = document.getElementById("title");
+  const crawlElement = document.getElementById('crawl')
+  crawlElement.classList.add('crawl')
+  const titleEl = document.getElementById('title')
 
-    const epNumEl = document.createElement("p");
-    epNumEl.id = "crawl-text";
-    epNumEl.innerHTML = `Episode ${romanNumEpisode}`;
-    titleEl.appendChild(epNumEl);
+  const epNumEl = document.createElement('p')
+  epNumEl.id = 'crawl-text'
+  epNumEl.innerHTML = `Episode ${romanNumEpisode}`
+  titleEl.appendChild(epNumEl)
 
-    const headingEl = document.createElement(`h1`);
-    headingEl.innerHTML = responseData.title;
-    titleEl.appendChild(headingEl);
+  const headingEl = document.createElement(`h1`)
+  headingEl.innerHTML = responseData.title
+  titleEl.appendChild(headingEl)
 
-    const crawlTextEl = document.createElement(`p`);
-    crawlTextEl.innerHTML = responseData.opening_crawl;
-    crawlElement.appendChild(crawlTextEl);
+  const crawlTextEl = document.createElement(`p`)
+  crawlTextEl.innerHTML = responseData.opening_crawl
+  crawlElement.appendChild(crawlTextEl)
 
   // Show menu
   setTimeout(() => {
-    stopPlayback();
-  }, 100000);
+    stopPlayback()
+  }, 100000)
 }
 
 /**
  * Stops playback by refreshing page
  */
 function stopPlayback() {
-  location.reload();
+  location.reload()
 }
 
 /**
  * Convert episode number to roman numerals
- * 
+ *
  * @param {number} number Episode number to convert
  * @returns Roman numeral equivalent
  */
 function convertToRomanNumeral(number) {
   switch (number) {
     case 1:
-      return "IV";
+      return 'IV'
     case 2:
-      return "V";
+      return 'V'
     case 3:
-      return "VI";
+      return 'VI'
     case 4:
-      return "I";
+      return 'I'
     case 5:
-      return "II";
+      return 'II'
     case 6:
-      return "III";
+      return 'III'
     default:
-      return "";
+      return ''
   }
 }
 
@@ -107,10 +107,10 @@ function convertToRomanNumeral(number) {
  * Toggles muted audio
  */
 function muteAudio() {
-  const audioEl = document.getElementById("audio");
+  const audioEl = document.getElementById('audio')
   if (!audioEl.muted) {
-    audioEl.muted = true;
+    audioEl.muted = true
   } else {
-    audioEl.muted = false;
+    audioEl.muted = false
   }
 }
